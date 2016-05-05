d28f3708efe239fa8dc1d4e34218dc50a55237e0
Alexander Lim <me@alexanderlim.com>
2016-05-05 00:17:06 -0400

    Finish exercise 6

--- 06-content-negotiation/index.js
+++ 06-content-negotiation/index.js
@@ -15,5 +15,13 @@ var gzip = require('mz/zlib').gzip;
 var app = module.exports = koa();
 
 app.use(function* () {
+    const body = 'hello world';
 
+    if (this.request.acceptsEncodings('gzip')) {
+        this.response.set('Content-Encoding', 'gzip');
+        this.response.body = yield gzip(body);
+    } else {
+        this.response.set('Content-Encoding', 'identity');
+        this.response.body = body;
+    }
 })
