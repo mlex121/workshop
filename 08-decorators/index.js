3dfdccfe248ad0b5369ea1b34bda5432e02fb67c
Alexander Lim <me@alexanderlim.com>
2016-05-05 00:26:13 -0400

    Finish exercise 8

--- 08-decorators/index.js
+++ 08-decorators/index.js
@@ -1,3 +1,16 @@
+var tagsToReplace = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;'
+};
+
+function replaceTag(tag) {
+    return tagsToReplace[tag] || tag;
+}
+
+function safe_tags_replace(str) {
+    return str.replace(/[&<>]/g, replaceTag);
+}
 
 var koa = require('koa');
 var escape = require('escape-html');
@@ -6,7 +19,7 @@ var app = module.exports = koa();
 
 app.use(function* (next) {
   yield next;
-  // add some logic here!
+  this.response.body = safe_tags_replace(this.response.body);
 })
 
 app.use(function* body() {
