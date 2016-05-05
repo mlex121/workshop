bf02e0ad6d299e7394d322b0df24920bdf5239d8
Alexander Lim <me@alexanderlim.com>
2016-05-05 00:22:27 -0400

    Finish exercise 7

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -4,5 +4,11 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* () {
-  
+  if (this.request.is('application/json') === 'application/json') {
+    // this.response.type = 'application/json';
+    // this.response.length =
+    this.response.body = { message: 'hi!' };
+  } else {
+    this.response.body = 'ok';
+  }
 })
